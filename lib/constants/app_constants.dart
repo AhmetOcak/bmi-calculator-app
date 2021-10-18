2db95fc5a1cd3f5441602c2491e6065d0dfb1f6d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-10-18 11:45:40 +0300

    inti

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,5 +2,5 @@
 
 import 'package:flutter/material.dart';
 
-final Color mainColor = Color.fromRGBO(13, 17, 55, 1.0);
-final Color accentColor = Color.fromRGBO(229, 33, 101, 1.0);
+const Color mainColor = Color.fromRGBO(13, 17, 55, 1.0);
+const Color accentColor = Color.fromRGBO(229, 33, 101, 1.0);
